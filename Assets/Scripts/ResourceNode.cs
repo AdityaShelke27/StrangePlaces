5e0ae60f48d80b3ee2af4947af2dfd75be88a723
Aditya Shelke <ShelkeAditya325@gmail.com>
2026-03-18 22:01:49 +0530

    Created basic node script and now working on machine

---
+++ Assets/Scripts/ResourceNode.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ResourceNode : MonoBehaviour
+{
+    [SerializeField] SurfaceNode m_NodeType;
+    [SerializeField] SurfaceNodeAmount m_NodeAmount;
+    int m_MaxAmount;
+    int m_AmountAvailable;
+
+    void Start()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        
+    }
+    void Initialize()
+    {
+        m_MaxAmount = ResourceData.ResourceAmount[m_NodeAmount];
+        m_AmountAvailable = m_MaxAmount;
+    }
+    public int FetchResource(int amount)
+    {
+        if(amount >= m_AmountAvailable)
+        {
+            m_AmountAvailable -= amount;
+        }
+        else
+        {
+            amount = m_AmountAvailable;
+            m_AmountAvailable = 0;
+        }
+
+        return amount;
+    }
+}
--- Assets/Scripts/ResourcesList.cs
+++ Assets/Scripts/ResourcesList.cs
@@ -1,7 +1,26 @@
+using System.Collections.Generic;
+
 public enum SurfaceNode
 {
     Ore,
     Plant,
     Gravitational_Anomaly,
     Alien_Artifact
+}
+public enum SurfaceNodeAmount
+{
+    Small,
+    Medium,
+    Large,
+    Very_Large
+}
+public static class ResourceData
+{
+    public static readonly Dictionary<SurfaceNodeAmount, int> ResourceAmount = new()
+    {
+        { SurfaceNodeAmount.Small, 10 },
+        { SurfaceNodeAmount.Medium, 25 },
+        { SurfaceNodeAmount.Large, 50 },
+        { SurfaceNodeAmount.Very_Large, 100 },
+    };
 }
